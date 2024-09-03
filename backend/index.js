c55baa3d4d78bffd1e76008f02cbc3da90c09979
devanagh007one <158443465+devanagh007one@users.noreply.github.com>
2024-09-03 15:36:07 +0530

    worker

--- backend/index.js
+++ backend/index.js
@@ -1,232 +1,216 @@
-// require('dotenv').config()
-const helmet = require('helmet')
+const cluster = require('cluster');
+const os = require('os');
+const helmet = require('helmet');
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv'); //chatgpt
+const dotenv = require('dotenv');
 dotenv.config();
-const app = express();
-require("./db/config");
-const User = require("./db/User");
-const reguser = require("./models/endUserModel");
-const vbrand = require("./models/brandModel");
-const path = require('path');
-const bcrypt = require('bcrypt');
 
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
 
-app.use(helmet());
-app.use(express.json());
-app.use(cors());
+  console.log(`Master ${process.pid} is running`);
 
-// 
-// app.use((req, res, next) => {
-//   res.removeHeader('X-Powered-By');
-//   // Add or modify any other headers here
-//   next();
-// });
-
-
-
-const brandImagesDirectory = path.join(__dirname, 'public/brandImages');
-const userImagesDirectory = path.join(__dirname, 'public/userImages');
-const productImagesDirectory = path.join(__dirname, 'public/productImages');
-const varientImagesDirectory = path.join(__dirname, 'public/varientImages');
-const ownersreviewImagesDirectory = path.join(__dirname, 'public/ownersreviewImages');
-app.use('/userImages', express.static(userImagesDirectory));
-app.use('/brandImages', express.static(brandImagesDirectory));
-app.use('/productImages', express.static(productImagesDirectory));
-app.use('/varientImages', express.static(varientImagesDirectory));
-app.use('/ownersImages', express.static(ownersreviewImagesDirectory));
-
-
-
-
-const saltRounds = 10;
-
-
-
-
-app.post("/register", async (req, res) => {
-  if (req.body.password && req.body.email) {
-    try {
-      
-      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-
-      
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword,
-        
-      });
-
-      
-      await newUser.save();
-     
-      
-      res.send({ result: 'User registered successfully' });
-    } catch (error) {
-      res.status(500).send({ result: 'Internal server error' });
-    }
-  } else {
-    res.status(400).send({ result: 'Invalid request' });
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-});
 
-app.post("/login", async (req, res) => {
-  if (req.body.password && req.body.email) {
-    try {
-      let user = await User.findOne({ email: req.body.email });
-
-      if (user) {
-        
-        bcrypt.compare(req.body.password, user.password, function (err, result) {
-          if (err) {
-            res.status(500).send({ result: 'Error comparing passwords' });
-          } else if (result) {
-            
-            res.send({ ...user._doc, password: undefined });
-          } else {
-            res.send({ result: 'Incorrect password' });
-          }
-        });
-      } else {
-        res.send({ result: 'No user found' });
-      }
-    } catch (error) {
-      res.status(500).send({ result: 'Internal server error' });
-    }
-  } else {
-    res.send({ result: 'No user found' });
-  }
-});
-
-//user router
-
-const user_routes = require("./routes/userRoute");
-app.use('/api',user_routes);
-
-app.get("/enduser",(req,res)=>{
-  reguser.find()
-  .then(reguser=>res.json(reguser))
-  .catch(err=>res.json(err))
-})
-
-app.get("/search/:key", async(req, res)=>{
-  let result = await reguser.find({
-    "$or":[
-      { fname:{$regex:req.params.key} },
-      { lname:{$regex:req.params.key} },
-      { email:{$regex:req.params.key} },
-      { mobile:{$regex:req.params.key} },
-      { pin:{$regex:req.params.key} },
-      { DOB:{$regex:req.params.key} },
-      { gender:{$regex:req.params.key} },
-      { profession:{$regex:req.params.key} },
-      { wheredidyouhearus:{$regex:req.params.key} }
-    ]
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork(); // Fork a new worker if one dies
   });
-  res.send(result);
-});
-app.post("/endUserlogin", async(req, res) => {
-  if (req.body.mobile){
-    let user = await reguser.findOne({ mobile: req.body.mobile });
-  if (user){
-    res.send(user);
-  } else {
-    res.send({result:'No User found'})
-  }
-  } 
-  else {
-    res.send({result:'No User found'})
-  }
-  
-});
+} else {
+  const app = express();
+  require("./db/config");
+  const User = require("./db/User");
+  const reguser = require("./models/endUserModel");
+  const vbrand = require("./models/brandModel");
+  const path = require('path');
+  const bcrypt = require('bcrypt');
 
-//Brand-API
-const brand_routes = require("./routes/brandRoute");
-const { fstat } = require('fs');
-app.use('/api',brand_routes);
+  app.use(helmet());
+  app.use(express.json());
+  app.use(cors());
 
-app.get("/brands",(req,res)=>{
-  vbrand.find()
-  .then(vbrand=>res.json(vbrand))
-  .catch(err=>res.json(err))
-})
+  const brandImagesDirectory = path.join(__dirname, 'public/brandImages');
+  const userImagesDirectory = path.join(__dirname, 'public/userImages');
+  const productImagesDirectory = path.join(__dirname, 'public/productImages');
+  const varientImagesDirectory = path.join(__dirname, 'public/varientImages');
+  const ownersreviewImagesDirectory = path.join(__dirname, 'public/ownersreviewImages');
 
-app.delete("/brands/:id",async(req,res)=>{
-  
-  const result = await vbrand.deleteOne({_id:req.params.id})
-  res.send(result);
-  
-})
+  app.use('/userImages', express.static(userImagesDirectory));
+  app.use('/brandImages', express.static(brandImagesDirectory));
+  app.use('/productImages', express.static(productImagesDirectory));
+  app.use('/varientImages', express.static(varientImagesDirectory));
+  app.use('/ownersImages', express.static(ownersreviewImagesDirectory));
 
-//common route
+  const saltRounds = 10;
 
-const common_route = require("./routes/commonRoute");
-app.use('/api', common_route);
-//Add Product
+  app.post("/register", async (req, res) => {
+    if (req.body.password && req.body.email) {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword,
+        });
 
-const product_route = require("./routes/productRoute");
-app.use('/api', product_route);
+        await newUser.save();
+        res.send({ result: 'User registered successfully' });
+      } catch (error) {
+        res.status(500).send({ result: 'Internal server error' });
+      }
+    } else {
+      res.status(400).send({ result: 'Invalid request' });
+    }
+  });
 
-//Edit and Mislanious operation for Product
-const mislanious_product = require("./routes/productExtraRoute");
-app.use('/api/extra', mislanious_product);
+  app.post("/login", async (req, res) => {
+    if (req.body.password && req.body.email) {
+      try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+          bcrypt.compare(req.body.password, user.password, function (err, result) {
+            if (err) {
+              res.status(500).send({ result: 'Error comparing passwords' });
+            } else if (result) {
+              res.send({ ...user._doc, password: undefined });
+            } else {
+              res.send({ result: 'Incorrect password' });
+            }
+          });
+        } else {
+          res.send({ result: 'No user found' });
+        }
+      } catch (error) {
+        res.status(500).send({ result: 'Internal server error' });
+      }
+    } else {
+      res.send({ result: 'No user found' });
+    }
+  });
 
-//Varient Route
-const varient_route = require("./routes/varientRoute");
-app.use('/api', varient_route);
+  // User router
+  const user_routes = require("./routes/userRoute");
+  app.use('/api', user_routes);
 
-//BRAND CSV- By mistake I have named it as user
-const brand_csv = require("./routes/brandCSVRoute");
-app.use('/api/v1', brand_csv);
-//VARIENT CSV- By mistake I have named it as user
-const varient_csv = require("./routes/varientCSVRoute");
-app.use('/api/v1', varient_csv);
-//PRODUCT CSV ROUTE
-const productCSV = require("./routes/productCSVRoute");
-app.use('/api/v1', productCSV);
+  app.get("/enduser", (req, res) => {
+    reguser.find()
+      .then(reguser => res.json(reguser))
+      .catch(err => res.json(err))
+  });
 
-//OWNER'S REVIEW ROUTE
-const ownersReview = require("./routes/ownersReviewRoute");
-app.use('/api/v1/tern',ownersReview);
+  app.get("/search/:key", async (req, res) => {
+    let result = await reguser.find({
+      "$or": [
+        { fname: { $regex: req.params.key } },
+        { lname: { $regex: req.params.key } },
+        { email: { $regex: req.params.key } },
+        { mobile: { $regex: req.params.key } },
+        { pin: { $regex: req.params.key } },
+        { DOB: { $regex: req.params.key } },
+        { gender: { $regex: req.params.key } },
+        { profession: { $regex: req.params.key } },
+        { wheredidyouhearus: { $regex: req.params.key } }
+      ]
+    });
+    res.send(result);
+  });
 
-//OTP ROUTES
-const otpsendandVerification = require("./routes/otpRoute");
-app.use('/api/auth',otpsendandVerification);
+  app.post("/endUserlogin", async (req, res) => {
+    if (req.body.mobile) {
+      let user = await reguser.findOne({ mobile: req.body.mobile });
+      if (user) {
+        res.send(user);
+      } else {
+        res.send({ result: 'No User found' });
+      }
+    } else {
+      res.send({ result: 'No User found' });
+    }
+  });
 
-//COMPARO ROUTES
-const comparoRoutes = require("./routes/comparoRoutes");
-app.use('/api/comparo',comparoRoutes);
+  // Brand-API
+  const brand_routes = require("./routes/brandRoute");
+  app.use('/api', brand_routes);
 
+  app.get("/brands", (req, res) => {
+    vbrand.find()
+      .then(vbrand => res.json(vbrand))
+      .catch(err => res.json(err))
+  });
 
-//DELERSHIP ROUTES
-const dealershipRoute = require("./routes/dealershipRoute");
-app.use('/api/v1',dealershipRoute)
+  app.delete("/brands/:id", async (req, res) => {
+    const result = await vbrand.deleteOne({ _id: req.params.id })
+    res.send(result);
+  });
 
-//SERVICE CENTRE ROUTES
-const service_Centre_routes = require("./routes/servicecentreRoute");
-app.use('/api/v1',service_Centre_routes)
+  // Common route
+  const common_route = require("./routes/commonRoute");
+  app.use('/api', common_route);
 
-//ASSIST ME ROUTES
-// const assistMe = require("./routes/assistmeRoute");
-// app.use('/api/assistme',assistMe);
+  // Add Product
+  const product_route = require("./routes/productRoute");
+  app.use('/api', product_route);
 
-//RTO DATA ROUTES
-const rtoData = require("./routes/rtodataRoutes");
-app.use('/api/v1',rtoData);
+  // Edit and Mislanious operation for Product
+  const mislanious_product = require("./routes/productExtraRoute");
+  app.use('/api/extra', mislanious_product);
 
-//FUEL PRICE ROUTE
-const fuelPrice = require("./routes/fuelpriceRoute");
-app.use('/api/v1',fuelPrice);
+  // Varient Route
+  const varient_route = require("./routes/varientRoute");
+  app.use('/api', varient_route);
 
-//PRICE BREAKUP PAGE LOAN 
-const loanCalculator = require("./routes/loanRoute");
-app.use('/api/v1',loanCalculator);
+  // BRAND CSV
+  const brand_csv = require("./routes/brandCSVRoute");
+  app.use('/api/v1', brand_csv);
 
-// Use process.env.PORT for the server's port
-const PORT = process.env.PORT || 5000; // Use 3000 as a default if PORT is not specified in .env
+  // VARIENT CSV
+  const varient_csv = require("./routes/varientCSVRoute");
+  app.use('/api/v1', varient_csv);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  // PRODUCT CSV ROUTE
+  const productCSV = require("./routes/productCSVRoute");
+  app.use('/api/v1', productCSV);
+
+  // OWNER'S REVIEW ROUTE
+  const ownersReview = require("./routes/ownersReviewRoute");
+  app.use('/api/v1/tern', ownersReview);
+
+  // OTP ROUTES
+  const otpsendandVerification = require("./routes/otpRoute");
+  app.use('/api/auth', otpsendandVerification);
+
+  // COMPARO ROUTES
+  const comparoRoutes = require("./routes/comparoRoutes");
+  app.use('/api/comparo', comparoRoutes);
+
+  // DELERSHIP ROUTES
+  const dealershipRoute = require("./routes/dealershipRoute");
+  app.use('/api/v1', dealershipRoute);
+
+  // SERVICE CENTRE ROUTES
+  const service_Centre_routes = require("./routes/servicecentreRoute");
+  app.use('/api/v1', service_Centre_routes);
+
+  // RTO DATA ROUTES
+  const rtoData = require("./routes/rtodataRoutes");
+  app.use('/api/v1', rtoData);
+
+  // FUEL PRICE ROUTE
+  const fuelPrice = require("./routes/fuelpriceRoute");
+  app.use('/api/v1', fuelPrice);
+
+  // PRICE BREAKUP PAGE LOAN
+  const loanCalculator = require("./routes/loanRoute");
+  app.use('/api/v1', loanCalculator);
+
+  // Use process.env.PORT for the server's port
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} is running on port ${PORT}`);
+  });
+}
